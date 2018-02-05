f28c7ae4bce0c002515cb9b79c2e0dad64e3f5e8
xieyan <xy_fds@163.com>
2018-02-05 14:47:51 +0800

    update

--- jk_web/src/main/java/com/xy/shiro/AuthRealm.java
+++ jk_web/src/main/java/com/xy/shiro/AuthRealm.java
@@ -1,26 +1,20 @@
 package com.xy.shiro;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import com.google.common.collect.Sets;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.AuthorizationInfo;
-import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.realm.AuthorizingRealm;
-import org.apache.shiro.subject.PrincipalCollection;
-
 import com.xy.domain.Module;
 import com.xy.domain.Role;
 import com.xy.domain.User;
 import com.xy.service.UserService;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class AuthRealm extends AuthorizingRealm{
 
