a1837879680577f0a2afbd9f3d5cfee935d08e86
xieyan <xy_fds@163.com>
2018-02-28 09:10:05 +0800

    update

---
+++ jk_web/src/test/java/com/xy/test/EnumTest.java
@@ -0,0 +1,17 @@
+package com.xy.test;
+
+/**
+ * @author xieyan
+ * @description
+ * @date 2018/1/23.
+ */
+public enum  EnumTest {
+    contractAction;
+    private String name;
+    private String curl;
+    private int number;
+    EnumTest(){
+
+    }
+}
+
